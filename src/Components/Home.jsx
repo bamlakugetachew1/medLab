2ab656b39900a370bf193006762fae9e51ddb4ac
bamlaku <abuyeget@gmail.com>
2025-07-29 13:52:26 +0300

    fixing

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ import Button from "../Layouts/Button";
 
 const Home = () => {
   return (
-    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white bg-[url('assets/img/home.png')] bg-no-repeat bg-cover opacity-90">
+    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white bg-[url('https://res.cloudinary.com/dwq2ftoo3/image/upload/v1753785129/Blogs/home_zz29qg.png')] bg-no-repeat bg-cover opacity-90">
       <div className=" w-full lg:w-4/5 space-y-5 mt-10">
         <h1 className="text-5xl font-bold leading-tight">
           Empowering Health Choices for a Vibrant Life...
